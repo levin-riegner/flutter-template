9162ebbd592a5bdb89f3929151c6d89ef55b987d
Alqueraf <alqueraf@gmail.com>
2021-09-21 16:59:31 +0200

    Clear Secure Storage on app install

--- lib/data/shared/service/local/user_config_service.dart
+++ lib/data/shared/service/local/user_config_service.dart
@@ -3,20 +3,21 @@ import 'package:shared_preferences/shared_preferences.dart';
 /// Use this class to store simple key-value preferences
 /// that will only be persisted locally during the user session
 class UserConfigService {
+  final SharedPreferences _sharedPreferences;
+
+  const UserConfigService(this._sharedPreferences);
+
   static const String _kDataCollectionEnabledKey = "dataCollectionEnabled";
 
   Future<bool> isDataCollectionEnabled() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    return prefs.getBool(_kDataCollectionEnabledKey) ?? false;
+    return _sharedPreferences.getBool(_kDataCollectionEnabledKey) ?? false;
   }
 
   Future<void> saveDataCollectionEnabled(bool isEnabled) async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    prefs.setBool(_kDataCollectionEnabledKey, isEnabled);
+    _sharedPreferences.setBool(_kDataCollectionEnabledKey, isEnabled);
   }
 
   Future<void> clear() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    await prefs.clear();
+    await _sharedPreferences.clear();
   }
 }
